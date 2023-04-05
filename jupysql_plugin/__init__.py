cc0954822d431c5e856e871ffb5a0db4a6733cca
Eduardo Blancas <github@blancas.io>
2023-04-05 17:15:44 -0600

    lint

--- jupysql_plugin/__init__.py
+++ jupysql_plugin/__init__.py
@@ -2,7 +2,4 @@ from ._version import __version__  # noqa: F401
 
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "jupysql-plugin"
-    }]
+    return [{"src": "labextension", "dest": "jupysql-plugin"}]
