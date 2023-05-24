a0aee91196ce0fc3433a03d72e54561e2f6ba344
Eduardo Blancas <github@blancas.io>
2023-05-24 12:16:30 -0600

    updates ui tests

--- ui-tests/tests/jupysql_plugin.test.ts
+++ ui-tests/tests/jupysql_plugin.test.ts
@@ -1,3 +1,4 @@
+// based on https://github.com/bqplot/bqplot/blob/master/ui-tests/tests/bqplot.test.ts
 import { IJupyterLabPageFixture, test } from '@jupyterlab/galata';
 import { expect } from '@playwright/test';
 import * as path from 'path';
@@ -10,7 +11,7 @@ const filterUpdateNotebooks = item => {
 }
 
 const testCellOutputs = async (page: IJupyterLabPageFixture, tmpPath: string, theme: 'JupyterLab Light' | 'JupyterLab Dark') => {
-    const paths = klaw(path.resolve(__dirname, './notebooks'), { filter: item => !filterUpdateNotebooks(item), nodir: true });
+    const paths = klaw(path.resolve(__dirname, '../notebooks'), { filter: item => !filterUpdateNotebooks(item), nodir: true });
     const notebooks = paths.map(item => path.basename(item.path));
 
     const contextPrefix = theme == 'JupyterLab Light' ? 'light' : 'dark';
@@ -18,8 +19,9 @@ const testCellOutputs = async (page: IJupyterLabPageFixture, tmpPath: string, th
 
     for (const notebook of notebooks) {
         let results = [];
+        console.log(`CHECKING NOTEBOOK: ${notebook}`)
 
-        await page.notebook.openByPath(`${tmpPath}/${notebook}`);
+        await page.notebook.openByPath(notebook);
         await page.notebook.activate(notebook);
 
         let numCellImages = 0;
@@ -48,83 +50,26 @@ const testCellOutputs = async (page: IJupyterLabPageFixture, tmpPath: string, th
     }
 }
 
-// const testPlotUpdates = async (page: IJupyterLabPageFixture, tmpPath: string, theme: 'JupyterLab Light' | 'JupyterLab Dark') => {
-//     const paths = klaw(path.resolve(__dirname, './notebooks'), { filter: item => filterUpdateNotebooks(item), nodir: true });
-//     const notebooks = paths.map(item => path.basename(item.path));
 
-//     const contextPrefix = theme == 'JupyterLab Light' ? 'light' : 'dark';
-//     page.theme.setTheme(theme);
 
-//     for (const notebook of notebooks) {
-//         let results = [];
-
-//         await page.notebook.openByPath(`${tmpPath}/${notebook}`);
-//         await page.notebook.activate(notebook);
-
-//         const getCaptureImageName = (contextPrefix: string, notebook: string, id: number): string => {
-//             return `${contextPrefix}-${notebook}-cell-${id}.png`;
-//         };
-
-//         let cellCount = 0;
-//         await page.notebook.runCellByCell({
-//             onAfterCellRun: async (cellIndex: number) => {
-//                 // Always get first cell output which must contain the plot
-//                 const cell = await page.notebook.getCellOutput(0);
-//                 if (cell) {
-//                     results.push(await cell.screenshot());
-//                     cellCount++;
-//                 }
-//             }
-//         });
-
-//         await page.notebook.save();
-
-//         for (let i = 0; i < cellCount; i++) {
-//             expect(results[i]).toMatchSnapshot(getCaptureImageName(contextPrefix, notebook, i));
-//         }
-
-//         await page.notebook.close(true);
-//     }
-// };
-
-test.describe('test', () => {
+test.describe('jupysql-plugin Visual Regression', () => {
     test.beforeEach(async ({ page, tmpPath }) => {
         page.on("console", (message) => {
             console.log('CONSOLE MSG ---', message.text());
         });
 
         await page.contents.uploadDirectory(
-            path.resolve(__dirname, './notebooks'),
+            path.resolve(__dirname, '../notebooks'),
             tmpPath
         );
         await page.filebrowser.openDirectory(tmpPath);
     });
 
-    test('Light theme: stuff', async ({
+    test('Light theme: Check jupysql-plugin first renders', async ({
         page,
         tmpPath,
     }) => {
         await testCellOutputs(page, tmpPath, 'JupyterLab Light');
     });
 
-    // test('Dark theme: Check bqplot first renders', async ({
-    //     page,
-    //     tmpPath,
-    // }) => {
-    //     await testCellOutputs(page, tmpPath, 'JupyterLab Dark');
-    // });
-
-    // test('Light theme: Check bqplot update plot properties', async ({
-    //     page,
-    //     tmpPath,
-    // }) => {
-    //     await testPlotUpdates(page, tmpPath, 'JupyterLab Light');
-    // });
-
-    // test('Dark theme: Check bqplot update plot properties', async ({
-    //     page,
-    //     tmpPath,
-    // }) => {
-    //     await testPlotUpdates(page, tmpPath, 'JupyterLab Dark');
-    // });
 });
