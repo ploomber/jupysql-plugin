ba897c69fd2499e20ab25ee89212052c5c7a8a36
Tony Kuo <yen.c@ploomber.io>
2023-06-20 13:49:10 -0400

    first fix

---
+++ jupysql-plugin/__init__.py
@@ -0,0 +1,25 @@
+from ._version import __version__  # noqa: F401
+from ploomber.jupyter.manager import _load_jupyter_server_extension
+
+_module_name = "jupysql-plugin"
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "jupysql-plugin"}]
+
+
+def _jupyter_server_extension_paths():
+    return [{'module': 'jupysql-plugin'}]
+
+def _load_jupyter_server_extension(server_app):
+    """Registers the API handler to receive HTTP requests from the frontend extension.
+
+    Parameters
+    ----------
+    server_app: jupyterlab.labapp.LabApp
+        JupyterLab application instance
+    """
+    # setup_handlers(server_app.web_app)
+    server_app.log.info(f"Registered {_module_name} server extension")
+
+load_jupyter_server_extension = _load_jupyter_server_extension
--- jupysql_plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._version import __version__  # noqa: F401
-
-_module_name = "jupysql-plugin"
-
-
-def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": "jupysql-plugin"}]
