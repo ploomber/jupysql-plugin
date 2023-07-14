e69a7ee5be9d6aa1d5a8ffd420e56b702f55aebb
Tony Kuo <yen.c@ploomber.io>
2023-07-13 20:38:13 -0400

    add test

--- src/__tests__/dialog.spec.tsx
+++ src/__tests__/dialog.spec.tsx
@@ -27,6 +27,16 @@ const mockPostAPI = (result: any, api_key: string) => {
     })
 }
 
+// If projectID is provided, the API is used for re-deployment
+const mockJobDeployResult = (projectID = "", deployment_result: object = {
+    project_id: DEFAULT_PROJECT_ID,
+    id: DEFAULT_JOB_ID
+}) => {
+    when((requestAPI as jest.Mock)).calledWith("job", { "body": `{\"api_key\":\"${DEFAULT_VALID_API_KEY}\",\"project_id\":\"${projectID}\"}`, "method": "POST" }).mockResolvedValue({
+        deployment_result: deployment_result
+    })
+}
+
 const renderDialogContent = (project_id: any = null) => {
     if (!project_id) {
         render(<DialogContent metadata={new Map()} />);
@@ -34,7 +44,6 @@ const renderDialogContent = (project_id: any = null) => {
         render(<DialogContent metadata={new Map([["ploomber", { project_id: project_id }]])} />);
     }
 }
-
 
 describe("Test DialogContent ", () => {
 
@@ -98,26 +107,29 @@ describe("Test DialogContent ", () => {
 
     describe("API Deploy Stage", () => {
 
-        beforeEach(() => jest.clearAllMocks());
-
-        test("Test First Time Deployment Layout", async () => {
+        /* Before:
+            1. Valid API Key saved in user's environment
+        */
+        beforeEach(() => {
+            jest.clearAllMocks();
             mockEnvWithAPI(DEFAULT_VALID_API_KEY);
+        });
+
+        /*
+        Ensure layout components render properly
+        */
+        test("Test First Time Deployment Layout", async () => {
             renderDialogContent();
 
             expect(await screen.findByText('Clicking on deploy will upload your notebook to Ploomber Cloud servers')).toBeVisible()
             expect(await screen.getByRole('button')).toHaveTextContent('CONFIRM')
 
         })
-
+        /* 
+        Test Flow: When the notebook have not been deployed before
+        */
         test("Test First Time Deployment Click Confim", async () => {
-            mockEnvWithAPI(DEFAULT_VALID_API_KEY);
-            when((requestAPI as jest.Mock)).calledWith("job", { "body": `{\"api_key\":\"${DEFAULT_VALID_API_KEY}\",\"project_id\":\"\"}`, "method": "POST" }).mockResolvedValue({
-                deployment_result: {
-                    project_id: DEFAULT_PROJECT_ID,
-                    id: DEFAULT_JOB_ID
-                }
-            })
-
+            mockJobDeployResult();
             renderDialogContent();
             expect(await screen.findByText('Clicking on deploy will upload your notebook to Ploomber Cloud servers')).toBeVisible()
             expect(await screen.getByRole('button')).toHaveTextContent('CONFIRM')
@@ -127,22 +139,35 @@ describe("Test DialogContent ", () => {
 
             expect(await screen.findByText("Check your deployment status here:")).toBeVisible()
             expect(await screen.findByText(`https://platform.ploomber.io/dashboards/${DEFAULT_PROJECT_ID}/${DEFAULT_JOB_ID}`)).toBeVisible()
-            screen.debug()
         })
-
-        test("Test Existing Project redeployment", async () => {
-            mockEnvWithAPI(DEFAULT_VALID_API_KEY);
-            when((requestAPI as jest.Mock)).calledWith("job", { "body": `{\"api_key\":\"${DEFAULT_VALID_API_KEY}\",\"project_id\":\"${DEFAULT_PROJECT_ID}\"}`, "method": "POST" }).mockResolvedValue({
-                deployment_result: {
-                    project_id: DEFAULT_PROJECT_ID,
-                    id: DEFAULT_JOB_ID
-                }
-            })
-
+        /* 
+        Test Flow: When the notebook have been deployed before
+        */
+        test("Test Existing Project Re-deployment Success", async () => {
+            mockJobDeployResult(DEFAULT_PROJECT_ID);
             renderDialogContent(DEFAULT_PROJECT_ID)
             expect(await screen.findByText("Check your deployment status here:")).toBeVisible()
             expect(await screen.findByText(`https://platform.ploomber.io/dashboards/${DEFAULT_PROJECT_ID}/${DEFAULT_JOB_ID}`)).toBeVisible()
-            screen.debug()
+        })
+        /* 
+        Test Flow: When the community user deploys more than one active project
+        */
+        test("Test Community Users Only One Project Fail", async () => {
+            mockJobDeployResult(DEFAULT_PROJECT_ID, {
+                "message": "Community users are only allowed to have a single active project. Delete your current project to create a new one."
+            });
+            renderDialogContent(DEFAULT_PROJECT_ID);
+            expect(await screen.findByText("Community users are only allowed to have a single active project. Delete your current project to create a new one.")).toBeVisible()
+        })
+        /* 
+        Test Flow: When the directiory is missing requirements.txt file
+        */
+        test("Test requirements.txt file is missing in directiory", async () => {
+            mockJobDeployResult(DEFAULT_PROJECT_ID, {
+                "detail": "Please make sure you have such file: /Users/tonykuo/requirements.txt"
+            });
+            renderDialogContent(DEFAULT_PROJECT_ID);
+            expect(await screen.findByText("Please make sure you have such file: /Users/tonykuo/requirements.txt")).toBeVisible()
         })
     })
 
