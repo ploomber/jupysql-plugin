c136460143490b8451554499c070bf1a7101a96c
Tony Kuo <yen.c@ploomber.io>
2023-07-18 21:49:11 -0400

    update deploy_sql.test.ts

--- ui-tests/tests/deploy_sql.test.ts
+++ ui-tests/tests/deploy_sql.test.ts
@@ -1,13 +1,10 @@
 import { test } from '@jupyterlab/galata';
 import { expect } from '@playwright/test';
 
-test('test deploy notebook', async ({ page }) => {
+test('test deploy notebook button is clickable', async ({ page }) => {
     await page.notebook.createNew("sample.ipynb");
     await page.notebook.openByPath("sample.ipynb");
     await page.notebook.activate("sample.ipynb");
-    await page.notebook.addCell("code", "%%sql\nselect * from table")
-    await page.getByTestId('format-btn').locator('button').click();
+    await page.getByTestId('-btn').locator('button').click();
     await page.waitForTimeout(2000);
-
-
 });
