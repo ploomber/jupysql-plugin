432d053ad7cefecf6cb56139b913ac774787cc52
Eduardo Blancas <github@blancas.io>
2023-08-16 18:36:44 -0600

    adds sample codeeditor plugin

---
+++ src/editor/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import {
+  EditorExtensionRegistry,
+  IEditorExtensionRegistry
+} from '@jupyterlab/codemirror';
+
+import { zebraStripes } from './editor';
+
+/**
+ * Initialization data for the @jupyterlab-examples/codemirror-extension extension.
+ */
+const plugin_editor: JupyterFrontEndPlugin<void> = {
+  id: '@jupyterlab-examples/codemirror-extension:plugin',
+  description: 'A minimal JupyterLab extension adding a CodeMirror extension.',
+  autoStart: true,
+  requires: [IEditorExtensionRegistry],
+  activate: (app: JupyterFrontEnd, extensions: IEditorExtensionRegistry) => {
+    // Register a new editor configurable extension
+    extensions.addExtension(
+      Object.freeze({
+        name: '@jupyterlab-examples/codemirror:zebra-stripes',
+        // Default CodeMirror extension parameters
+        default: 2,
+        factory: () =>
+          // The factory will be called for every new CodeMirror editor
+          EditorExtensionRegistry.createConfigurableExtension((step: number) =>
+              zebraStripes({ step })
+          ),
+        // JSON schema defining the CodeMirror extension parameters
+        schema: {
+          type: 'number',
+          title: 'Show stripes',
+          description: 'Display zebra stripes every "step" in CodeMirror editors.'
+        }
+      })
+    );
+  }
+};
+
+export { plugin_editor };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { plugin_syntax_highlight } from './syntax-highlight/index';
 import { plugin_completer } from './completer/index';
+import { plugin_editor } from './editor/index';
 import { plugin_formatting } from './formatter/index';
 import { plugin_widget } from './widgets/index';
 import { plugin_settings } from './settings/index';
@@ -8,6 +9,7 @@ export * from './version';
 export default [
   plugin_completer,
   plugin_syntax_highlight,
+  plugin_editor,
   plugin_formatting,
   plugin_widget,
   plugin_settings
