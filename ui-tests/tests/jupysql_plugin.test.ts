29ba0d16b756cf4cd3d6c74ffdf690b945b85cae
Eduardo Blancas <github@blancas.io>
2023-08-18 10:21:49 -0600

    adds syntaxt highlighting screenshot, refactors tests

--- ui-tests/tests/jupysql_plugin.test.ts
+++ ui-tests/tests/jupysql_plugin.test.ts
@@ -5,11 +5,6 @@ import * as path from 'path';
 const klaw = require('klaw-sync');
 
 
-const filterUpdateNotebooks = item => {
-    const basename = path.basename(item.path);
-    return basename.includes('_update');
-}
-
 /**
 Returns .ipynb files that are not in hidden directories
 */
@@ -20,49 +15,51 @@ const filterNotebooks = item => {
     );
 };
 
-const testCellOutputs = async (page: IJupyterLabPageFixture, tmpPath: string, theme: 'JupyterLab Light' | 'JupyterLab Dark') => {
-    const paths = klaw(path.resolve(__dirname, '../notebooks'), { filter: item => filterNotebooks(item), nodir: true });
-    const notebooks = paths.map(item => path.basename(item.path));
-
+const testCells = async (page: IJupyterLabPageFixture, notebook: string, theme: 'JupyterLab Light' | 'JupyterLab Dark', check_output: boolean = true) => {
     const contextPrefix = theme == 'JupyterLab Light' ? 'light' : 'dark';
     page.theme.setTheme(theme);
 
-    for (const notebook of notebooks) {
-        let results = [];
-        console.log(`Testing notebook: ${notebook}`)
+    let results = [];
+    console.log(`Testing notebook: ${notebook}`)
 
-        await page.notebook.openByPath(notebook);
-        await page.notebook.activate(notebook);
+    await page.notebook.openByPath(notebook);
+    await page.notebook.activate(notebook);
 
-        let numCellImages = 0;
+    let numCellImages = 0;
 
-        const getCaptureImageName = (contextPrefix: string, notebook: string, id: number): string => {
-            return `${contextPrefix}-${notebook}-cell-${id}.png`;
-        };
+    const getCaptureImageName = (contextPrefix: string, notebook: string, id: number): string => {
+        return `${contextPrefix}-${notebook}-cell-${id}.png`;
+    };
 
-        await page.notebook.runCellByCell({
-            onAfterCellRun: async (cellIndex: number) => {
-                const cell = await page.notebook.getCellOutput(cellIndex);
-                if (cell) {
-                    results.push(await cell.screenshot());
-                    numCellImages++;
-                }
+    await page.notebook.runCellByCell({
+        onAfterCellRun: async (cellIndex: number) => {
+            let cell
+
+            if (check_output) {
+                cell = await page.notebook.getCellOutput(cellIndex);
+            } else {
+                cell = await page.notebook.getCellInput(cellIndex);
             }
-        });
 
-        await page.notebook.save();
-
-        for (let c = 0; c < numCellImages; ++c) {
-            expect(results[c]).toMatchSnapshot(getCaptureImageName(contextPrefix, notebook, c));
+            if (cell) {
+                results.push(await cell.screenshot());
+                numCellImages++;
+            }
         }
+    });
 
-        await page.notebook.close(true);
+    await page.notebook.save();
+
+    for (let c = 0; c < numCellImages; ++c) {
+        expect(results[c]).toMatchSnapshot(getCaptureImageName(contextPrefix, notebook, c));
     }
+
+    await page.notebook.close(true);
 }
 
 
 
-test.describe('jupysql-plugin Visual Regression', () => {
+test.describe('jupysql-plugin ui-test', () => {
     test.beforeEach(async ({ page, tmpPath }) => {
         page.on("console", (message) => {
             console.log('CONSOLE MSG ---', message.text());
@@ -75,11 +72,21 @@ test.describe('jupysql-plugin Visual Regression', () => {
         await page.filebrowser.openDirectory(tmpPath);
     });
 
-    test('Light theme: Check jupysql-plugin first renders', async ({
-        page,
-        tmpPath,
-    }) => {
-        await testCellOutputs(page, tmpPath, 'JupyterLab Light');
+    const paths = klaw(path.resolve(__dirname, '../notebooks'), { filter: item => filterNotebooks(item), nodir: true });
+    const notebooks = paths.map(item => path.basename(item.path));
+
+    notebooks.forEach(notebook => {
+        test(`jupysql-plugin ui-test for notebook: "${notebook}"`, async ({
+            page,
+        }) => {
+            if (notebook.startsWith('input')) {
+                await testCells(page, notebook, 'JupyterLab Light', false);
+            } else {
+                await testCells(page, notebook, 'JupyterLab Light', true);
+            }
+
+        });
     });
+
 
 });
