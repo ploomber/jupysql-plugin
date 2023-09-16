1c7715126ce8d2a3bf1cef520ed86df602e00f9b
Eduardo Blancas <github@blancas.io>
2023-09-16 15:19:34 -0600

    settings plugin

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { plugin_syntax_highlight } from './syntax-highlight/index';
 import { plugin_completer } from './completer/index';
 import { plugin_formatting } from './formatter/index';
 import { plugin_widget } from './widgets/index';
+import { plugin_settings } from './settings/index';
 
 
 export * from './version';
-export default [plugin_completer, plugin_syntax_highlight, plugin_formatting, plugin_widget];
+export default [plugin_completer, plugin_syntax_highlight, plugin_formatting, plugin_widget, plugin_settings];
---
+++ src/settings/index.ts
@@ -0,0 +1,80 @@
+import {
+    JupyterFrontEnd,
+    JupyterFrontEndPlugin,
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+const PLUGIN_ID = 'jupysql-plugin:settings';
+const COMMAND_ID = 'jupysql-plugin:toggle-flag';
+
+/**
+ * Initialization data for the settings extension.
+ */
+const plugin_settings: JupyterFrontEndPlugin<void> = {
+    id: PLUGIN_ID,
+    autoStart: true,
+    requires: [ISettingRegistry],
+    activate: (app: JupyterFrontEnd, settings: ISettingRegistry) => {
+        const { commands } = app;
+        let limit = 25;
+        let flag = false;
+
+        /**
+         * Load the settings for this extension
+         *
+         * @param setting Extension settings
+         */
+        function loadSetting(setting: ISettingRegistry.ISettings): void {
+            // Read the settings and convert to the correct type
+            limit = setting.get('limit').composite as number;
+            flag = setting.get('flag').composite as boolean;
+
+            console.log(
+                `Settings Example extension: Limit is set to '${limit}' and flag to '${flag}'`
+            );
+        }
+
+        // Wait for the application to be restored and
+        // for the settings for this plugin to be loaded
+        Promise.all([app.restored, settings.load(PLUGIN_ID)])
+            .then(([, setting]) => {
+                // Read the settings
+                loadSetting(setting);
+
+                // Listen for your plugin setting changes using Signal
+                setting.changed.connect(loadSetting);
+
+                commands.addCommand(COMMAND_ID, {
+                    label: 'Toggle Flag and Increment Limit',
+                    isToggled: () => flag,
+                    execute: () => {
+                        // Programmatically change a setting
+                        Promise.all([
+                            setting.set('flag', !flag),
+                            setting.set('limit', limit + 1),
+                        ])
+                            .then(() => {
+                                const newLimit = setting.get('limit').composite as number;
+                                const newFlag = setting.get('flag').composite as boolean;
+                                window.alert(
+                                    `Settings Example extension: Limit is set to '${newLimit}' and flag to '${newFlag}'`
+                                );
+                            })
+                            .catch((reason) => {
+                                console.error(
+                                    `Something went wrong when changing the settings.\n${reason}`
+                                );
+                            });
+                    },
+                });
+            })
+            .catch((reason) => {
+                console.error(
+                    `Something went wrong when reading the settings.\n${reason}`
+                );
+            });
+    },
+};
+
+export { plugin_settings }
