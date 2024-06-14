18d9d785e99b05a038ccafdff1129dc58b201894
neelasha23 <neelasha.sen@gmail.com>
2024-06-14 14:46:29 +0530

    timeouts

--- ui-tests/tests/format_sql.test.ts
+++ ui-tests/tests/format_sql.test.ts
@@ -7,7 +7,7 @@ test('test format SQL', async ({ page }) => {
     await page.notebook.activate("sample.ipynb");
     await page.notebook.addCell("code", "%%sql\nselect * from table")
     await page.getByTestId('format-btn').locator('button').click();
-    await page.waitForTimeout(2000);
+    await page.waitForTimeout(5000);
 
     await expect(page.locator('body')).toContainText('SELECT');
     await expect(page.locator('body')).toContainText('FROM');
